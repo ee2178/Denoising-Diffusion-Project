dcac90da2b1fafdf8b5cacc61cfaad40fbfbff6e
Eric Eng <ee2178@gr003.hpc.nyu.edu>
2025-11-07 16:42:46 -0500

    new photos

--- my_CDLNet/job.sh
+++ my_CDLNet/job.sh
@@ -7,7 +7,7 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=32GB
 #SBATCH --time=24:00:00
-#SBATCH --job-name=CDLNet-MRI-ImgRescaling1e6_Stride_2
+#SBATCH --job-name=CDLNet-MRI-ImgRescaling1e9_Stride_2
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=ee2178@nyu.edu
 #SBATCH --output=slurm_%j.out
