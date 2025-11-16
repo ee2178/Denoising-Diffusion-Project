ecbc3812dbcb760ac399fee3b5901159922b8f21
Eric Eng <ee2178@gv012.hpc.nyu.edu>
2025-11-16 18:17:31 -0500

    sense works!

--- my_CDLNet/mri_utils.py
+++ my_CDLNet/mri_utils.py
@@ -30,7 +30,7 @@ def mri_decoding(y, acceleration_map, smaps):
     # Apply mask to each channel of y
     y_mask = y * acceleration_map[None]
     # Apply ifft2
-    x_coils = fft.fftshift(fft.ifft2(fft.ifftshift(y, dim = (1, 2)), norm = 'ortho'), dim = (1, 2))
+    x_coils = fft.fftshift(fft.ifft2(fft.ifftshift(y_mask, dim = (1, 2)), norm = 'ortho'), dim = (1, 2))
     # Coil combination
     x = torch.einsum("ijk, ijk -> jk", smaps.conj(), x_coils)
     return x
