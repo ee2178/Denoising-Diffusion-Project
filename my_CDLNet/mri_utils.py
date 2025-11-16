541b522d7c012f4a15f52aa9a09b67f796894cc8
Eric Eng <ee2178@gv012.hpc.nyu.edu>
2025-11-16 17:49:23 -0500

    pain

--- my_CDLNet/mri_utils.py
+++ my_CDLNet/mri_utils.py
@@ -30,7 +30,7 @@ def mri_decoding(y, acceleration_map, smaps):
     # Apply mask to each channel of y
     y_mask = y * acceleration_map[None]
     # Apply ifft2
-    x_coils = fft.fftshift(fft.ifft2(y, norm = 'ortho'), dim = (-2, -1))
+    x_coils = (fft.ifft2(y, norm = 'ortho', dim = (-2, -1)))
     # Coil combination
     x = torch.einsum("ijk, ijk -> jk", smaps.conj(), x_coils)
     return x
