596f34296f4b7d944e048e65f82f44bae0b59b7b
Eric Eng <ee2178@gr003.hpc.nyu.edu>
2025-12-01 14:28:13 -0500

    sorted iterations in gif properly

--- my_CDLNet/make_gif.py
+++ my_CDLNet/make_gif.py
@@ -1,6 +1,6 @@
 import imageio.v2 as imageio
 import os
-
+import re
 import argparse
 
 parser = argparse.ArgumentParser()
@@ -8,11 +8,18 @@ parser.add_argument("--image_path", type = str, help="Corresponding path where i
 parser.add_argument("--gifname", type = str, help="What to name your gif after saving", default = None)
 args = parser.parse_args() 
 
+def natural_sort_key(filename):
+    # Extract numerical parts from the filename
+    parts = [int(c) if c.isdigit() else c for c in re.split(r'(\d+)', filename)]
+    return parts
+
 def make_gif(dir, gifname):
-    filenames = os.listdir(dir)
+    filenames = sorted(os.listdir(dir), key = natural_sort_key)
     with imageio.get_writer(gifname, mode='I') as writer:
         for filename in filenames:
             image = imageio.imread(os.path.join(dir, filename))
+            writer.append_data(image)
+            writer.append_data(image)
             writer.append_data(image)
 
 if __name__ == "__main__":
