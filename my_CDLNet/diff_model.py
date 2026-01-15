076524f5e23d17811dbf517d304dbad31b8bd8a2
Eric Eng <ee2178@gr004.hpc.nyu.edu>
2026-01-15 15:35:35 -0500

    testing stopping thresholds for mixed immap2

--- my_CDLNet/diff_model.py
+++ my_CDLNet/diff_model.py
@@ -267,7 +267,6 @@ class ImMAP(nn.Module):
         # Precompute the noise schedule first
         sig_t_vec = [1]
         i=1
-        self.h_0=0.05
         while sig_t_vec[-1] > 0.01:
             sig_t_vec.append((1-self.beta * self.h_0 * i/(1+self.h_0*(i-1)))*sig_t_vec[i-1])
             i=i+1
@@ -298,7 +297,7 @@ class ImMAP(nn.Module):
         if mode == 2:
             # This version only does the e2e conditioning for a single step at the start
             with torch.no_grad():
-                while sigma_t > 0.05:
+                while sigma_t > 0.03:
                     sigma_t = sig_t_vec[t-1]
                     # update step size
                     h_t = self.h_0 * t/(1+self.h_0*(t-1))
@@ -439,7 +438,7 @@ def main():
 
     immap = ImMAP(net)
     # test = immap.forward_3(kspace_masked, noise_level, mask, smaps, save_dir)
-    test = immap.forward_2_e2econditioned(kspace_masked, noise_level, mask, smaps, lpdsnet, save_dir = save_dir, verbose = True, mode=1)
+    test = immap.forward_2_e2econditioned(kspace_masked, noise_level, mask, smaps, lpdsnet, save_dir = save_dir, verbose = True, mode=2)
     breakpoint()
 
 if __name__ == "__main__":
